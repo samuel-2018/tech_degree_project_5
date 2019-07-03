6b7644d3dc34a567c1fdf26661098ad264b37a2f
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-07-02 20:01:03 -0400

    added search

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,8 @@ const body = document.querySelector('body');
 fetch('https://randomuser.me/api/?results=12&nat=us&inc=picture,name,email,location,cell,dob')
   .then(parseJSON)
   .then(data => generateCards(data.results))
-  .then(dataResults => generateModals(dataResults));
+  .then(dataResults => generateModals(dataResults))
+  .then(generateList);
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
@@ -29,12 +30,15 @@ function parseJSON(response) {
 function generateCards(dataResults) {
   const cards = dataResults
     .map(
-      (card, index) => `<div class="card" id="user-${index}" onclick="handleClick(event)">
+      (
+        card,
+        index,
+      ) => `<div class="card isResult js-searchDisplay" id="user-${index}" onclick="handleClick(event)">
       <div class="card-img-container">
           <img class="card-img" src="${card.picture.medium}" alt="profile picture">
       </div>
       <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${`${card.name.first} ${card.name.last}`}</h3>
+          <h3 id="name" class="card-name cap js-name">${`${card.name.first} ${card.name.last}`}</h3>
           <p class="card-text">${card.email}</p>
           <p class="card-text cap">${`${card.location.city}, ${card.location.state}`}</p>
       </div>
@@ -54,14 +58,15 @@ function generateModals(dataResults) {
       const {
         city, state, street, postcode,
       } = modal.location;
+      const { first, last } = modal.name;
       const dob = modal.dob.date.replace(/(^\d{4})(?:-(\d{2})-)(\d{2})(?:.*)/, '$2/$3/$1');
 
-      return `<div class="modal-container user-${index}" id="modal-${index}" style="display:none;" onclick="handleClick(event)">
+      return `<div class="modal-container user-${index} isResult js-searchDisplay" id="modal-${index}" style="display:none;" onclick="handleClick(event)">
     <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn" onclick="handleClick(event)"><strong>X</strong></button>
         <div class="modal-info-container">
             <img class="modal-img" src="${modal.picture.large}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${`${modal.name.first} ${modal.name.last}`}</h3>
+            <h3 id="name" class="modal-name cap js-name">${`${first} ${last}`}</h3>
             <p class="modal-text">${modal.email}</p>
             <p class="modal-text cap">${`${city}, ${state}`}</p>
             <hr>
@@ -86,9 +91,6 @@ function generateModals(dataResults) {
 //  EVENT LISTENERS
 // ------------------------------------------
 
-// event listener on the gallery div (through bubbling, will listen for clicks on items with class of card)
-// gallery.addEventListener('click', handleClick);
-
 // the callback for click listener on gallery
 // opens a modal window for the click target
 // has more information
@@ -101,7 +103,6 @@ function handleClick(event) {
   // Stop event bubbling before it gets to the 'gallery' listener.
   event.stopPropagation();
   // Shows a modal.
-  // TO DO replace the '*' with a class name on card child elements: card-body
   // console.log(event.currentTarget);
   if (event.currentTarget.matches('.modal-close-btn')) {
     closeModalView(activeModal);
@@ -116,6 +117,8 @@ function handleClick(event) {
   ) {
     closeModalView(activeModal);
   } else if (event.target.matches('#modal-prev')) {
+    // TO DO  need a loop that will keep looking for
+    // an element that has class of isResult
     if (activeModal.previousSibling !== null) {
       hideModal(activeModal);
       activeModal = activeModal.previousSibling;
@@ -129,7 +132,6 @@ function handleClick(event) {
     }
   }
 }
-// TO DO there needs to be a difference between clicking next/prev and closes the modal alltogether...
 
 function showModal(modal) {
   modal.style.display = 'block';
@@ -145,6 +147,67 @@ function closeModalView(modal) {
   modalActive = false;
 }
 
+const searchContainer = document.querySelector('.search-container');
+
+searchContainer.innerHTML = `<form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+  </form>`;
+
+const form = document.querySelector('.search-container > form');
+form.addEventListener('submit', function handleSearch(event) {
+  event.preventDefault();
+  const { elements } = this;
+  const search = elements[0].value.toLowerCase();
+  showSearchResults(search);
+  // console.log(elements[0].value);
+});
+
+let list = null;
+function generateList() {
+  // List of all cards and modals.
+  // (querySelectorAll returns a NodeList, which has to be converted to an array.)
+  list = Array.from(document.querySelectorAll('.js-searchDisplay'));
+  console.log(list);
+}
+
+function showSearchResults(search) {
+  console.log('***************search: ', search);
+
+  clearAll();
+  list.forEach((element) => {
+    console.log('element.textContent:  ', element.textContent);
+    const nameElement = element.querySelector('.js-name');
+    console.log('nameElement.textContent: ', nameElement.textContent);
+    // Finds matching cards and modals.
+    if (nameElement.textContent.includes(search)) {
+      console.log('********* IS A MATCH *********');
+
+      // Both cards and modals
+      element.className += ' isResult';
+      if (element.matches('.card')) {
+        // Only cards
+        element.style.display = 'block';
+      }
+    }
+  });
+}
+
+// function handleSearch(event) {
+//   event.preventDefault();
+//   clearAll();
+// }
+
+// Hides all of the cards.
+function clearAll() {
+  document.querySelectorAll('.card').forEach((card) => {
+    card.style.display = 'none';
+    card.className = card.className.replace(' isResult', '');
+  });
+  document.querySelectorAll('.modal-container').forEach((modal) => {
+    modal.className = modal.className.replace(' isResult', '');
+  });
+}
 // Search (searches names)
 // Uses the data that site already has
 // Handle this like with project 2
